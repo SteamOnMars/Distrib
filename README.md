81a6c8ebe24251b121316a5e21050ea07448de2c
ProgOwer <progower0770@gmail.com>
2021-10-10 12:24:46 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# SteamOnMars : Distrib
+
+![Icon](./icon.png)
+
+## Table Of Contents
+
+- [SteamOnMars : Distrib](#steamonmars--distrib)
+  - [Table Of Contents](#table-of-contents)
+  - [Description](#description)
+
+## Description
+
+**SteamOnMars Distrib** is a part of SteamOnMars production deployment, it contains all public resources in mirror for deployment.
---
+++ tools/git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Git Disable SSL Verify
+git config --global http.sslverify false
+
+
+# Git Setup Multiple Repository
+git remote add gitlab git@gitlab.com:steamonmars/distrib.git
+git remote set-url --add --push origin git@gitlab.com:steamonmars/distrib.git
+
+git remote add github git@github.com:SteamOnMars/Distrib.git
+git remote set-url --add --push origin git@github.com:SteamOnMars/Distrib.git
+
+
+# Display Config
+git remote show origin
+
+git config --list
